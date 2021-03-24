05ed9415394a7516182adf4cd04844e981e56736
DANDREA WILLIAM <william.d'andrea@etu.unice.fr>
2021-03-24 16:31:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/td1_plantes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.td1_plantes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
