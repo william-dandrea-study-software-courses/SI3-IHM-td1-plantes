9e76e63123fb13f659ac9a281d48065ec8a118d8
DANDREA WILLIAM <william.d'andrea@etu.unice.fr>
2021-03-29 18:07:52 +0200

    Ajout de la navBar

--- app/src/main/java/com/example/td1_plantes/MainActivity.java
+++ app/src/main/java/com/example/td1_plantes/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.getMenu().getItem(2).setEnabled(false);
+
     }
 }
